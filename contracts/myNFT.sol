bdd119b5f0732d31fba229c3d7c783ece6d4e780
abhayworld123 <abhayworld123@gmail.com>
2021-09-27 13:07:34 +0530

    first commit

---
+++ contracts/myNFT.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "hardhat/console.sol";
+
+contract myNFT is ERC721URIStorage {
+
+ using Counters for Counters.Counter;
+ Counters.Counter private _tokenIds;
+
+     // We need to pass the name of our NFTs token and it's symbol.
+  constructor() ERC721 ("GokuNFT", "TGoku") {
+    console.log("This is my NFT contract. Woah!");
+  }
+
+   // A function our user will hit to get their NFT.
+  function makeAnEpicNFT() public {
+     // Get the current tokenId, this starts at 0.
+    uint256 newItemId = _tokenIds.current();
+
+     // Actually mint the NFT to the sender using msg.sender.
+    _safeMint(msg.sender, newItemId);
+
+    // Set the NFTs data.
+    // _setTokenURI(newItemId, "https://jsonkeeper.com/b/14WI");
+
+    _setTokenURI(newItemId, "data:application/json;base64,eyJuYW1lIjoiVGV4dCBORlQiLCJkZXNjcmlwdGlvbiI6IlRoaXMgaXMgdGV4dCBuZnQuIiwiaW1hZ2UiOiJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStUbVYyWlhKSFpYUk1iM04wUEM5MFpYaDBQZ284TDNOMlp6ND0ifQ==");
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+
+    // Increment the counter for when the next NFT is minted.
+    _tokenIds.increment();
+  }
+}
